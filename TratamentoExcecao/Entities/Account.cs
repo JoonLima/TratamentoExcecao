599535142e4bbafda2270260bf73c864f628d49f
Jonathan Ventura <jonathanlventura3@gmail.com>
2022-05-13 23:16:53 -0300

    Primeiro commit

---
+++ TratamentoExcecao/Entities/Account.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using TratamentoExcecao.Entities.Exceptions;
+
+namespace TratamentoExcecao.Entities
+{
+    class Account
+    {
+        public int NumeroConta { get; set; }
+        public string NomeProprietario { get; set; }
+        public double Saldo { get; set; }
+        public double LimiteSaque { get; set; }
+
+        public Account(int numeroConta, string nomeProprietario, double saldo, double limiteSaque)
+        {
+            NumeroConta = numeroConta;
+            NomeProprietario = nomeProprietario;
+            Saldo = saldo;
+            LimiteSaque = limiteSaque;
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Saque(double valor)
+        {
+            if (valor > Saldo)
+            {
+                throw new DomainException("Você não possui saldo.");
+            }
+            if (valor > LimiteSaque)
+            {
+                throw new DomainException("Este valor excede o limite de saque.");
+            }
+
+            Saldo -= valor;
+        }
+
+
+
+    }
+}
---
+++ TratamentoExcecao/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TratamentoExcecao.Entities;
+using TratamentoExcecao.Entities.Exceptions;
+
+namespace TratamentoExcecao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.WriteLine("Entre com os dados da conta:");
+                Console.Write("Número: ");
+                int numeroConta = int.Parse(Console.ReadLine());
+                Console.Write("Proprietário: ");
+                string nomeProprietario = Console.ReadLine();
+                Console.Write("Saldo inicial: ");
+                double saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Limite de saque: ");
+                double limiteSaque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account account = new Account(numeroConta, nomeProprietario, saldo, limiteSaque);
+
+                Console.WriteLine();
+                Console.Write("Entre com o valor para saque: ");
+                account.Saque(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+                Console.WriteLine();
+                Console.WriteLine($"Novo saldo: {account.Saldo.ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine($"Erro no saque: {e.Message}");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Erro no saque: {e.Message}");
+            }
+        }
+    }
+}
